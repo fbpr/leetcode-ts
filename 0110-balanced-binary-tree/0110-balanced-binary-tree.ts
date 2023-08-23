c233eeea4324a56435f267eccf2992ee1b3c4050
Febry Prasetya <71777049+fbpr@users.noreply.github.com>
2023-08-23 16:48:44 +0700

    Time: 68 ms (73.5%), Space: 49.9 MB (17.42%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.ts
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.ts
@@ -90,44 +90,4 @@ function isBalanced(root: TreeNode | null): boolean {
         }
     }   
     return true;
-
-
-
-
-
-
-//   let ls: [TreeNode, number][] = [[root.left, root.left ? 1 : 0]];
-//   let rs: [TreeNode, number][] = [[root.right, root.right ? 1 : 0]];
-//   let lh: number = 0, rh: number = 0;
-
-//   while (ls.length) {
-//     let [node, currH]: [TreeNode, number] = ls.pop();
-//     lh = Math.max(lh, currH);
-//     if (node) {
-//       if (node.left) {
-//         ls.push([node.left, currH + 1]);
-//       }
-      
-//       if (node.right) {
-//         ls.push([node.right, currH + 1])
-//       }
-//     }
-//   }
-
-//   while (rs.length) {
-//     let [node, currH]: [TreeNode, number] = rs.pop();
-//     rh = Math.max(rh, currH);
-
-//     if (node) {
-//       if (node.left) {
-//         rs.push([node.left, currH + 1]);
-//       }
-
-//       if (node.right) {
-//         rs.push([node.right, currH + 1]);
-//       }
-//     }
-//   }
-
-//   return Math.abs(lh - rh) > 1 ? false : true;
 };
